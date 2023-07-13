df3fbdf29b775addcd49eff0f6e94f98ebc9a468
williamtgoss <williamgoss@arista.com>
2023-07-13 14:00:32 -0500

    update mac.sh

--- L3LS-Single-DC/mac.sh
+++ L3LS-Single-DC/mac.sh
@@ -6,11 +6,11 @@ sudo sed -i 's/..:..:..:..:..:../00:1c:73:ae:00:00/' clab-single-dc-l3ls/dc1-lea
 sudo sed -i 's/..:..:..:..:..:../00:1c:73:af:00:00/' clab-single-dc-l3ls/dc1-leaf2b/flash/system_mac_address
 sudo sed -i 's/..:..:..:..:..:../00:1c:73:b0:00:00/' clab-single-dc-l3ls/dc1-leaf1c/flash/system_mac_address
 sudo sed -i 's/..:..:..:..:..:../00:1c:73:b1:00:00/' clab-single-dc-l3ls/dc1-leaf2c/flash/system_mac_address
-sudo cp configs/cv-onboarding-token clab-single-dc-l3ls/dc1-spine1/flash/cv-onboarding-token
-sudo cp configs/cv-onboarding-token clab-single-dc-l3ls/dc1-spine2/flash/cv-onboarding-token
-sudo cp configs/cv-onboarding-token clab-single-dc-l3ls/dc1-leaf1a/flash/cv-onboarding-token
-sudo cp configs/cv-onboarding-token clab-single-dc-l3ls/dc1-leaf1b/flash/cv-onboarding-token
-sudo cp configs/cv-onboarding-token clab-single-dc-l3ls/dc1-leaf2a/flash/cv-onboarding-token
-sudo cp configs/cv-onboarding-token clab-single-dc-l3ls/dc1-leaf2b/flash/cv-onboarding-token
-sudo cp configs/cv-onboarding-token clab-single-dc-l3ls/dc1-leaf1c/flash/cv-onboarding-token
-sudo cp configs/cv-onboarding-token clab-single-dc-l3ls/dc1-leaf2c/flash/cv-onboarding-token
+sudo cp configs/cv-onboarding-token ../../clab-single-dc-l3/dc1-spine1/flash/cv-onboarding-token
+sudo cp configs/cv-onboarding-token ../../clab-single-dc-l3/dc1-spine2/flash/cv-onboarding-token
+sudo cp configs/cv-onboarding-token ../../clab-single-dc-l3/dc1-leaf1a/flash/cv-onboarding-token
+sudo cp configs/cv-onboarding-token ../../clab-single-dc-l3/dc1-leaf1b/flash/cv-onboarding-token
+sudo cp configs/cv-onboarding-token ../../clab-single-dc-l3/dc1-leaf2a/flash/cv-onboarding-token
+sudo cp configs/cv-onboarding-token ../../clab-single-dc-l3/dc1-leaf2b/flash/cv-onboarding-token
+sudo cp configs/cv-onboarding-token ../../clab-single-dc-l3/dc1-leaf1c/flash/cv-onboarding-token
+sudo cp configs/cv-onboarding-token ../../clab-single-dc-l3/dc1-leaf2c/flash/cv-onboarding-token
